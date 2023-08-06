433aad78c80b0b0ddc488e0ba24d4527678f248c
Shreyansh <shreyansh.ku.singh@gmail.com>
2023-08-06 09:18:10 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,129 @@
+let allMusic = [
+   
+    {
+        name: "Aya re khilone wala",
+        artist: "mohammad rafi",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "mere samne wali khidki me ",
+        artist: "RD burman",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "unchi unchi baaton se  ",
+        artist: "mohammad rafi",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Yeh Reshmi Zulfen",
+        artist: "Mohammad rafi",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Sidha sadha bnda",
+        artist: "Manish kumar",
+        img: "music-21",
+        src: "music-21"
+    },
+    {
+        name: "Chand si mehabooba ho ",
+        artist: "Kalyan Ji- Anand Ji",
+        img: "music-23",
+        src: "music-23"
+    },
+    {
+        name: "Likhe jo Khat tujhe",
+        artist: "Mohammad Rafi",
+        img: "music-27",
+        src: "music-27"
+    },
+    {
+        name: "Jina yha Marna yha",
+        artist: "Mukesh",
+        img: "music-28",
+        src: "music-28"
+    },
+    {
+        name: "Phirkiwali",
+        artist: "Mohammad Rafi",
+        img: "music-29",
+        src: "music-29"
+    },
+    {
+        name: "Tu Kitni achi h",
+        artist: "Lata Mangeshkar",
+        img: "music-30",
+        src: "music-30"
+    },
+    {
+        name: "Saagar Jaisi Aankhonwali",
+        artist: "Kishore kumar",
+        img: "music-31",
+        src: "music-31"
+    },
+    {
+        name: "Maine Dekha Ek Sapna",
+        artist: "Lata Mangeshkar",
+        img: "music-32",
+        src: "music-32"
+    },
+    {
+        name: "Ek Din Bik Jayega Mati Ke Mol ",
+        artist: "Mukesh",
+        img: "music-33",
+        src: "music-33"
+    },
+    {
+        name: "Duniya Bananewale",
+        artist: "Mukesh",
+        img: "music-34",
+        src: "music-34"
+    },
+    {
+        name: "Dil Ka Haal Sune Dilwala ",
+        artist: "Manna",
+        img: "music-35",
+        src: "music-35"
+    },
+    {
+        name: "Dheere Dheere Bol Koi Sun Na L",
+        artist: "Mukesh,Lata Mangeshkar",
+        img: "music-36",
+        src: "music-36"
+    },
+    {
+        name: "Chup Gaye Sare Nazare",
+        artist: "Mohammad rafi,Lata Mangeshkar",
+        img: "music-37",
+        src: "music-37"
+    },
+    {
+        name: "Chal Chal Chal Mere Saathi",
+        artist: "Kishor",
+        img: "music-38",
+        src: "music-38"
+    },
+    {
+        name: "Nafrat Ki Duniya Ko Chhod Ke",
+        artist: "Mohammad rafi",
+        img: "music-39",
+        src: "music-39"
+    },
+    {
+        name: "Duniya Mein Rahena Hai To ",
+        artist: "Kishor, Latamangeshkar",
+        img: "music-40",
+        src: "music-40"
+    },
+    {
+        name: "Sunja Aa Thandi Hawa",
+        artist: "Kishor, Latamangeshkar",
+        img: "music-41",
+        src: "music-41"
+    },
+];
